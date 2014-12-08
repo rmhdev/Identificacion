e3727f6aa961212c3c9aa2c117b1e530a7efed85
rober <rmh.dev@gmail.com>
2014-12-08 14:00:32 +0100

    More short code tests

--- tests/Identificacion/Tests/Code/DniCodeTest.php
+++ tests/Identificacion/Tests/Code/DniCodeTest.php
@@ -15,10 +15,26 @@ class DniIdentityTest extends \PHPUnit_Framework_TestCase
         new DniCode("123456789");
     }
 
-    public function testTooShortIdentityMustBeFilledWithTrailingZeros()
+    /**
+     * @dataProvider tooShortCodeProvider
+     * @param string $expected
+     * @param string $value
+     */
+    public function testTooShortCodeMustBeFilledWithTrailingZeros($expected, $value)
     {
-        $dniCode = new DniCode("123");
+        $dniCode = new DniCode($value);
 
-        $this->assertEquals("00000123", $dniCode->__toString());
+        $this->assertEquals($expected, $dniCode->__toString());
+    }
+
+    public function tooShortCodeProvider()
+    {
+        return array(
+            array("00000123", "123"),
+            array("00000123", "0123"),
+            array("00000000", ""),
+            array("00000000", null),
+
+        );
     }
 }
