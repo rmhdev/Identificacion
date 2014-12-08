15c5ea1b672a2634ad79000b9e9cedf1dd728dd0
rober <rmh.dev@gmail.com>
2014-12-08 14:55:05 +0100

    Test Nie letter

--- src/Identificacion/Code/NieCode.php
+++ src/Identificacion/Code/NieCode.php
@@ -3,11 +3,37 @@
 namespace Identificacion\Code;
 
 use Identificacion\Exception\LengthException;
+use Identificacion\Exception\UnexpectedValueException;
 
 class NieCode
 {
-    public function __construct($code)
+    const LENGTH = 7;
+
+    private static $letters = array("X", "Y", "Z");
+
+    private $code;
+    private $letter;
+
+    public function __construct($letter, $code)
     {
-        throw new LengthException();
+        $this->setLetter($letter);
+        $this->setCode($code);
+    }
+
+    private function setLetter($letter)
+    {
+        if (!in_array(strtoupper($letter), self::$letters)) {
+            throw new UnexpectedValueException();
+        }
+        $this->letter = $letter;
+    }
+
+    private function setCode($code)
+    {
+        if (self::LENGTH < strlen($code)) {
+            throw new LengthException();
+        }
+
+        $this->code = $code;
     }
 }
--- tests/Identificacion/Tests/Code/NieCodeTest.php
+++ tests/Identificacion/Tests/Code/NieCodeTest.php
@@ -13,4 +13,24 @@ class NieCodeTest extends \PHPUnit_Framework_TestCase
     {
         new NieCode("X", "12345678");
     }
+
+    /**
+     * @dataProvider unexpectedLetterProvider
+     * @expectedException \Identificacion\Exception\UnexpectedValueException
+     * @param string $value
+     */
+    public function testUnexpectedLetterMustThrowException($value)
+    {
+        new NieCode($value, "1234567");
+    }
+
+    public function unexpectedLetterProvider()
+    {
+        return array(
+            array("A"), array("B"), array("C"),
+            array(""),
+            array("XX"), array("XY"), array("ZZ"),
+        );
+    }
+
 }
